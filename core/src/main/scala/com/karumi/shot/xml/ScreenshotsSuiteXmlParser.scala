5e43e763b7bfc7dd5d673532a910b2d768aa1b35
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-07-21 11:50:36 +0200

    ScreenshotSuite xml parser finished and covered with tests

--- core/src/main/scala/com/karumi/shot/xml/ScreenshotsSuiteXmlParser.scala
+++ core/src/main/scala/com/karumi/shot/xml/ScreenshotsSuiteXmlParser.scala
@@ -1,11 +1,34 @@
 package com.karumi.shot.xml
 
-import com.karumi.shot.domain.model.{FilePath, ScreenshotsSuite}
+import com.karumi.shot.domain.Screenshot
+import com.karumi.shot.domain.model.ScreenshotsSuite
+
+import scala.xml._
 
 class ScreenshotsSuiteXmlParser {
 
   def parseScreenshots(xml: String): ScreenshotsSuite = {
-    Seq()
+    val xmlScreenshots = XML.loadString(xml) \ "screenshot"
+    xmlScreenshots.map(parseScreenshot)
+  }
+
+  private def parseScreenshot(xmlNode: Node): Screenshot = {
+    val name = (xmlNode \ "name" head).text
+    val testClass = (xmlNode \ "test_class" head).text
+    val testName = (xmlNode \ "test_name" head).text
+    val tileWidth = (xmlNode \ "tile_width" head).text.toInt
+    val tileHeight = (xmlNode \ "tile_height" head).text.toInt
+    val viewHierarchy = (xmlNode \ "view_hierarchy" head).text
+    val absoluteFileNames = (xmlNode \ "absolute_file_name").map(_.text)
+    val relativeFileNames = (xmlNode \ "relative_file_name").map(_.text)
+    Screenshot(name,
+               testClass,
+               testName,
+               tileWidth,
+               tileHeight,
+               viewHierarchy,
+               absoluteFileNames,
+               relativeFileNames)
   }
 
 }
--- core/src/test/scala/com/karumi/shot/xml/ScreenshotsSuiteXmlParserSpec.scala
+++ core/src/test/scala/com/karumi/shot/xml/ScreenshotsSuiteXmlParserSpec.scala
@@ -18,4 +18,35 @@ class ScreenshotsSuiteXmlParserSpec
 
     screenshots shouldBe empty
   }
+
+  it should "parse a regular metadata file" in {
+    val xml = testResourceContent("/screenshots-metadata/metadata.xml")
+
+    val screenshots = parser.parseScreenshots(xml)
+
+    screenshots.size shouldBe 11
+    val firstScreenshot = screenshots.head
+    firstScreenshot.name shouldBe "com.karumi.screenshot.MainActivityTest_showsSuperHeroesIfThereAreSomeSuperHeroes"
+    firstScreenshot.testClass shouldBe "com.karumi.screenshot.MainActivityTest"
+    firstScreenshot.testName shouldBe "showsSuperHeroesIfThereAreSomeSuperHeroes"
+    firstScreenshot.tileWidth shouldBe 2
+    firstScreenshot.tileHeight shouldBe 3
+    firstScreenshot.viewHierarchy shouldBe "com.karumi.screenshot.MainActivityTest_showsSuperHeroesIfThereAreSomeSuperHeroes_dump.xml"
+    firstScreenshot.absoluteFileNames shouldBe Seq(
+      "/mnt/sdcard/screenshots/com.karumi.screenshot.test/screenshots-default/com.karumi.screenshot.MainActivityTest_showsSuperHeroesIfThereAreSomeSuperHeroes.png",
+      "/mnt/sdcard/screenshots/com.karumi.screenshot.test/screenshots-default/com.karumi.screenshot.MainActivityTest_showsSuperHeroesIfThereAreSomeSuperHeroes_0_1.png",
+      "/mnt/sdcard/screenshots/com.karumi.screenshot.test/screenshots-default/com.karumi.screenshot.MainActivityTest_showsSuperHeroesIfThereAreSomeSuperHeroes_0_2.png",
+      "/mnt/sdcard/screenshots/com.karumi.screenshot.test/screenshots-default/com.karumi.screenshot.MainActivityTest_showsSuperHeroesIfThereAreSomeSuperHeroes_1_0.png",
+      "/mnt/sdcard/screenshots/com.karumi.screenshot.test/screenshots-default/com.karumi.screenshot.MainActivityTest_showsSuperHeroesIfThereAreSomeSuperHeroes_1_1.png",
+      "/mnt/sdcard/screenshots/com.karumi.screenshot.test/screenshots-default/com.karumi.screenshot.MainActivityTest_showsSuperHeroesIfThereAreSomeSuperHeroes_1_2.png"
+    )
+    firstScreenshot.relativeFileNames shouldBe Seq(
+      "com.karumi.screenshot.MainActivityTest_showsSuperHeroesIfThereAreSomeSuperHeroes.png",
+      "com.karumi.screenshot.MainActivityTest_showsSuperHeroesIfThereAreSomeSuperHeroes_0_1.png",
+      "com.karumi.screenshot.MainActivityTest_showsSuperHeroesIfThereAreSomeSuperHeroes_0_2.png",
+      "com.karumi.screenshot.MainActivityTest_showsSuperHeroesIfThereAreSomeSuperHeroes_1_0.png",
+      "com.karumi.screenshot.MainActivityTest_showsSuperHeroesIfThereAreSomeSuperHeroes_1_1.png",
+      "com.karumi.screenshot.MainActivityTest_showsSuperHeroesIfThereAreSomeSuperHeroes_1_2.png"
+    )
+  }
 }
