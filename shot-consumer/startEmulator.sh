92fd9d7b5d25f5b1eb7245e793203dcde95c7fe5
Pedro Vicente Gómez Sánchez <pedro@gokarumi.com>
2017-08-21 15:26:32 +0200

    Adjust emulator and record screenshots again

--- shot-consumer/startEmulator.sh
+++ shot-consumer/startEmulator.sh
@@ -2,6 +2,6 @@
 
 echo "Starting screenshot tests emulator 📸📱!"
 
-$ANDROID_HOME/emulator/emulator "-avd" "tests-emulator" "-no-boot-anim" &
+$ANDROID_HOME/emulator/emulator "-avd" "tests-emulator" "-skin" "480x800"  "-no-boot-anim" "-no-audio" &
 
 echo "Emulator ready. Hapy testing 😃"
