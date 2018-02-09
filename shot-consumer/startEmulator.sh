81f1d6e5cbe20435f8fe244b61ce967ed3677e82
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-02-09 14:51:40 +0100

    Update emulator size

--- shot-consumer/startEmulator.sh
+++ shot-consumer/startEmulator.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "Starting screenshot tests emulator 📸📱!"
 
-$ANDROID_HOME/emulator/emulator -avd "shot-emulator" -skin 768x1280 -no-audio -no-boot-anim &
+$ANDROID_HOME/emulator/emulator -avd "shot-emulator" -skin 1080x1920 -no-audio -no-boot-anim &
 
 echo "Emulator ready. Happy testing 😃"
