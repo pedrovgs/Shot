e1b79118c7f799de2f607615a75c0a3f7b7fe834
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-02-09 12:30:22 +0100

    Update device config and remove travis support

--- shot-consumer/createEmulator.sh
+++ shot-consumer/createEmulator.sh
@@ -9,7 +9,7 @@ echo "SDCard created ✅"
 
 echo "Creating a tests-emulator 📱"
 
-echo no | $ANDROID_HOME/tools/bin/avdmanager --verbose create avd --force --name "shot-test-emulator" --abi default/x86_64 --package "system-images;android-23;default;x86_64" --sdcard sdcard.img --device 'Nexus 4'
+echo no | $ANDROID_HOME/tools/bin/avdmanager --verbose create avd --force --name "shot-test-emulator" --abi default/x86_64 --package "system-images;android-26;default;x86_64" --sdcard sdcard.img --device 'Nexus 5X'
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp $DIR/config.ini ~/.android/avd/shot-test-emulator.avd/config.ini
