4006aab3dac594b42e573bbbb9625666e13a5e42
diegolucasb <diegolucasb@gmail.com>
2018-10-02 16:26:39 -0300

    Removes @LargeTest that was removed from support library

--- shot-consumer/app/src/androidTest/java/com/karumi/screenshot/ScreenshotTest.java
+++ shot-consumer/app/src/androidTest/java/com/karumi/screenshot/ScreenshotTest.java
@@ -19,16 +19,16 @@ package com.karumi.screenshot;
 import android.app.Activity;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
-import android.test.suitebuilder.annotation.LargeTest;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.WindowManager;
+
 import com.facebook.testing.screenshot.Screenshot;
 import com.facebook.testing.screenshot.ViewHelpers;
 
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 
-@LargeTest public class ScreenshotTest {
+public class ScreenshotTest {
 
   protected void compareScreenshot(Activity activity) {
     Screenshot.snapActivity(activity).record();
