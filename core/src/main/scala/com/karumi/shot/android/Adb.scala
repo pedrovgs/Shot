39aa248b4b14a41e5ebf4f8ebdb72cfa3cab255c
Vinicius Guardieiro Sousa <viniciusguardieiro@gmail.com>
2020-03-20 11:16:56 -0300

    solve requests

--- core/src/main/scala/com/karumi/shot/android/Adb.scala
+++ core/src/main/scala/com/karumi/shot/android/Adb.scala
@@ -13,23 +13,13 @@ class Adb {
   final val CR_ASCII_DECIMAL = 13
 
   def devices: List[String] = {
-    listWithoutCarriageReturn(executeAdbCommandWithResult("devices").split('\n').toList.drop(1).map {
+    executeAdbCommandWithResult("devices").split('\n').toList.drop(1).map {
       line =>
         line.split('\t').toList.head
-    })
+    }.filter(device => !isCarriageReturnASCII(device))
   }
 
-  def listWithoutCarriageReturn(strings: List[String]): List[String] = {
-    var result = List[String]()
-    strings.foreach{
-      line =>
-        if(!isCarriageReturnASCII(line))
-          result = line :: result
-    }
-    result
-  }
-
-  def isCarriageReturnASCII(device: String): Boolean = device.charAt(0) == CR_ASCII_DECIMAL
+  private def isCarriageReturnASCII(device: String): Boolean = device.charAt(0) == CR_ASCII_DECIMAL
 
   def pullScreenshots(device: String,
                       screenshotsFolder: Folder,
