74a583ae4119677bdbc1dcd64f10d8164be5db8a
Héctor Abraham Morillo Prieto <hectorabrahammp@gmail.com>
2020-04-16 19:41:33 +0200

    Implemented new activity with just an editText
    This activity will be used to test that shot-android disable cursor on edittext before make the screenshot.

---
+++ shot-consumer/app/src/androidTest/java/com/karumi/ui/view/CursorActivityTest.kt
@@ -0,0 +1,24 @@
+package com.karumi.ui.view
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.github.salomonbrys.kodein.Kodein
+import com.karumi.R
+import org.junit.Test
+
+class CursorActivityTest : AcceptanceTest<CursorActivity>(
+        CursorActivity::class.java
+) {
+
+    @Test
+    fun cursorIsNotVisibleOnScreenshot() {
+        val activity = startActivity()
+        onView(withId(R.id.et_cursor)).perform(click())
+        onView(withId(R.id.et_cursor)).perform(typeText("Cursor Test"))
+        compareScreenshot(activity)
+    }
+
+    override val testDependencies = Kodein.Module(allowSilentOverride = true) {}
+}
