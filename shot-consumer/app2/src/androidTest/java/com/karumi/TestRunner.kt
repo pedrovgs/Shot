a5ccb0149d06d264a8fa138cace11627abcf02f7
Héctor Abraham Morillo Prieto <hectorabrahammp@gmail.com>
2020-04-16 19:24:49 +0200

    Removed unnecessary imports

--- shot-consumer/app2/src/androidTest/java/com/karumi/TestRunner.kt
+++ shot-consumer/app2/src/androidTest/java/com/karumi/TestRunner.kt
@@ -2,9 +2,6 @@ package com.karumi
 
 import android.app.Application
 import android.content.Context
-import android.os.Bundle
-import androidx.test.runner.AndroidJUnitRunner
-import com.facebook.testing.screenshot.ScreenshotRunner
 import com.github.tmurakami.dexopener.DexOpener
 import com.karumi.shot.ShotTestRunner
 
@@ -22,5 +19,4 @@ class TestRunner : ShotTestRunner() {
         app.asApp().kodein.mutable = true
         super.callApplicationOnCreate(app)
     }
-
 }
