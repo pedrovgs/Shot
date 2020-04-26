1a07aa5d4ae0b1aafa1aecd762b771e8c0df0797
Héctor Abraham Morillo Prieto <hectorabrahammp@gmail.com>
2020-04-26 20:57:35 +0200

    Removed unnecessary code

--- shot-consumer/app/src/main/java/com/karumi/ui/presenter/ScrollPresenter.kt
+++ shot-consumer/app/src/main/java/com/karumi/ui/presenter/ScrollPresenter.kt
@@ -1,17 +1,8 @@
 package com.karumi.ui.presenter
 
-import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleObserver
-import android.arch.lifecycle.OnLifecycleEvent
-import com.karumi.common.weak
 
-class ScrollPresenter(view: View) : LifecycleObserver {
+class ScrollPresenter : LifecycleObserver {
 
-    private val view: View? by weak(view)
-
-    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
-    fun update() {
-    }
-
-    interface View {}
+    interface View
 }
