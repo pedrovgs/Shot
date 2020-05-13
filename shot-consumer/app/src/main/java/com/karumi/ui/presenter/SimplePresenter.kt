7d636924a47c3037f3b74f3d1f7ee5305ca54d7a
Alex Krupa <alexk6@outlook.com>
2020-05-13 22:13:27 +0200

    Add a test comparing an Activity screenshot with custom name

---
+++ shot-consumer/app/src/main/java/com/karumi/ui/presenter/SimplePresenter.kt
@@ -0,0 +1,8 @@
+package com.karumi.ui.presenter
+
+import android.arch.lifecycle.LifecycleObserver
+
+class SimplePresenter : LifecycleObserver {
+
+    interface View
+}
---
+++ shot-consumer/app/src/main/java/com/karumi/ui/view/SimpleActivity.kt
@@ -0,0 +1,24 @@
+package com.karumi.ui.view
+
+import android.support.v7.widget.Toolbar
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.bind
+import com.github.salomonbrys.kodein.instance
+import com.github.salomonbrys.kodein.provider
+import com.karumi.R
+import com.karumi.ui.presenter.SimplePresenter
+import kotlinx.android.synthetic.main.simple_activity.*
+
+class SimpleActivity : BaseActivity(), SimplePresenter.View {
+
+    override val layoutId: Int = R.layout.simple_activity
+    override val presenter: SimplePresenter by injector.instance()
+    override val toolbarView: Toolbar
+        get() = toolbar
+
+    override val activityModules = Kodein.Module(allowSilentOverride = true) {
+        bind<SimplePresenter>() with provider {
+            SimplePresenter()
+        }
+    }
+}
