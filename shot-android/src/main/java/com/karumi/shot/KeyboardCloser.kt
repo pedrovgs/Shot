17392f522aa6317a31ac7b2770c41a154c77d626
Pedro Vicente Gómez Sánchez <pedrovgs@users.noreply.github.com>
2020-07-08 12:24:12 +0200

    KeyboardCloser interface (#127)
    * Fount the best implementation possible
    
    * Update implementation to include a max number of retries as Davide suggested

---
+++ shot-android/src/main/java/com/karumi/shot/KeyboardCloser.kt
@@ -0,0 +1,51 @@
+package com.karumi.shot
+
+import android.R
+import android.app.Activity
+import android.graphics.Rect
+import android.os.Build
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.annotation.RequiresApi
+import kotlin.math.absoluteValue
+
+@RequiresApi(Build.VERSION_CODES.CUPCAKE)
+interface KeyboardCloser {
+
+    fun closeKeyboardAndWaitUntilIsNotVisible(activity: Activity) {
+        recursiveCloseKeyboardAndWaitUntilIsNotVisible(activity = activity)
+    }
+
+    fun closeKeyboard(activity: Activity) {
+        val focusedView = activity.currentFocus
+        if (focusedView != null) {
+            activity.runOnUiThread {
+                val imm: InputMethodManager =
+                        activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(focusedView.windowToken, 0)
+            }
+        }
+    }
+
+    private fun recursiveCloseKeyboardAndWaitUntilIsNotVisible(activity: Activity, retriesLeft: Int = 5) {
+        if (retriesLeft == 0) {
+            throw IllegalStateException("We couldn't close they keyboard after 5 retries.")
+        }
+        closeKeyboard(activity)
+        val rootView = activity.findViewById<View>(R.id.content).rootView
+        val measuredRect = Rect()
+        rootView.rootView.getWindowVisibleDisplayFrame(measuredRect)
+        val rootViewHeight = measuredRect.bottom - measuredRect.top
+        val screenHeight = rootView.rootView.height
+        val heightDiff: Int = (screenHeight - rootViewHeight).absoluteValue
+        val isKeyboardClosed = heightDiff < (screenHeight * 0.05)
+        if (!isKeyboardClosed) {
+            sleep(500)
+            recursiveCloseKeyboardAndWaitUntilIsNotVisible(activity, retriesLeft - 1)
+        }
+    }
+
+    private fun sleep(millis: Long) {
+        Thread.sleep(millis)
+    }
+}
--- shot-consumer/app/src/androidTest/java/com/karumi/ui/view/CursorActivityTest.kt
+++ shot-consumer/app/src/androidTest/java/com/karumi/ui/view/CursorActivityTest.kt
@@ -2,20 +2,21 @@ package com.karumi.ui.view
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.github.salomonbrys.kodein.Kodein
 import com.karumi.R
+import com.karumi.shot.KeyboardCloser
 import org.junit.Test
 
 class CursorActivityTest : AcceptanceTest<CursorActivity>(
         CursorActivity::class.java
-) {
+), KeyboardCloser {
 
     @Test
     fun cursorIsNotVisibleOnScreenshot() {
         val activity = startActivity()
         onView(withId(R.id.et_cursor)).perform(click())
+        closeKeyboardAndWaitUntilIsNotVisible(activity)
         compareScreenshot(activity)
     }
 
