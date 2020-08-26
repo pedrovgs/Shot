318b2e2b2b0c8e6431adcc8cdd92f6ca659b7a23
Pedro Vicente Gómez Sánchez <pedro@karumi.com>
2020-08-26 15:17:23 +0200

    Prepare next development iteration

--- core/src/main/scala/com/karumi/shot/domain/model.scala
+++ core/src/main/scala/com/karumi/shot/domain/model.scala
@@ -20,7 +20,7 @@ object Config {
   val androidDependencyMode: FilePath = "androidTestImplementation"
   val androidDependencyGroup: String = "com.karumi"
   val androidDependencyName: String = "shot-android"
-  val androidDependencyVersion: String = "4.4.0"
+  val androidDependencyVersion: String = "4.4.1-SNAPSHOT"
   val androidDependency: FilePath =
     s"$androidDependencyGroup:$androidDependencyName:$androidDependencyVersion"
   def screenshotsFolderName(flavor: String, buildType: String): FilePath =
