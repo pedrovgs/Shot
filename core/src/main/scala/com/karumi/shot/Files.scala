95959462019225774a2d2657289d2c2aee03dc4f
Pedro Vicente Gómez Sánchez <pedrovgs@users.noreply.github.com>
2020-10-05 11:56:55 +0200

    Improve error handling when there are only compose screenshots (#155)
    * Improve error handling when there are only compose screenshots
    
    * Prevent list files to fail if the folder does not exist

--- core/src/main/scala/com/karumi/shot/Files.scala
+++ core/src/main/scala/com/karumi/shot/Files.scala
@@ -9,11 +9,21 @@ import org.apache.commons.io.FileUtils
 import scala.io.Source
 
 class Files {
-  def listFilesInFolder(folder: FilePath): util.Collection[File] =
-    FileUtils.listFiles(new File(folder), null, false)
+  def listFilesInFolder(folder: FilePath): util.Collection[File] = {
+    val file = new File(folder)
+    if (file.exists()) {
+      FileUtils.listFiles(file, null, false)
+    } else {
+      util.Collections.emptyList()
+    }
+  }
 
-  def rename(origin: FilePath, target: FilePath): Unit =
-    FileUtils.moveFile(new File(origin), new File(target))
+  def rename(origin: FilePath, target: FilePath): Unit = {
+    val originFile = new File(origin)
+    if (originFile.exists()) {
+      FileUtils.moveFile(originFile, new File(target))
+    }
+  }
 
   def read(filePath: FilePath): String =
     Source.fromFile(filePath, "UTF8").getLines.mkString
