8cb8df28ea2828cfc1e3885acb83aba3a1218ad2
Pedro Vicente Gómez Sánchez <pedrovgs@users.noreply.github.com>
2020-11-16 11:38:07 +0100

    Update all the tooling to the latest compose release (#173)
    * Update all the tooling to the latest release
    
    * Finish alpha-7 migration
    
    * Fix build error by adding packaging options
    
    * Add missing packaging options configuration
    
    * Move extension to an object so we don't face issues while sharing it

--- shot-android/src/main/java/com/karumi/shot/compose/SemanticsNodeBitmapGenerator.kt
+++ shot-android/src/main/java/com/karumi/shot/compose/SemanticsNodeBitmapGenerator.kt
@@ -3,10 +3,11 @@ package com.karumi.shot.compose
 import android.graphics.Bitmap
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.ui.test.captureToBitmap
+import androidx.compose.ui.graphics.asAndroidBitmap
+import androidx.compose.ui.test.captureToImage
 
 class SemanticsNodeBitmapGenerator {
 
     @RequiresApi(Build.VERSION_CODES.O)
-    fun generateBitmap(screenshot: ScreenshotSource.Node): Bitmap = screenshot.node.captureToBitmap()
+    fun generateBitmap(screenshot: ScreenshotSource.Node): Bitmap = screenshot.node.captureToImage().asAndroidBitmap()
 }
