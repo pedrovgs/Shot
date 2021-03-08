574ae5d31fb7c0759a14af705e29343312e11bb8
Pedro Gómez <pedro@karumi.com>
2021-03-08 18:24:54 +0100

    Revert duplicated folder changes to use only one

--- shot-android/src/main/java/com/karumi/shot/AndroidStorageInfo.kt
+++ shot-android/src/main/java/com/karumi/shot/AndroidStorageInfo.kt
@@ -1,6 +1,5 @@
 package com.karumi.shot
 
-import android.os.Build
 import android.os.Environment
 
 object AndroidStorageInfo {
@@ -8,10 +7,6 @@ object AndroidStorageInfo {
     // in the device we can use without handling the scoped storage API. Before using
     // this folder we used "/sdcard
     val storageBaseUrl: String by lazy {
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath
-        } else {
-            "/sdcard"
-        }
+        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath
     }
 }
