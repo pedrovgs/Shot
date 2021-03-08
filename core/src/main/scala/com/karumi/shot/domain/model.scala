ba8c07d90910ec0e663cc22e53f4af39fd0773ec
Pedro Gómez <pedro@karumi.com>
2021-03-08 18:46:38 +0100

    Prepare release 5.10.3

--- core/src/main/scala/com/karumi/shot/domain/model.scala
+++ core/src/main/scala/com/karumi/shot/domain/model.scala
@@ -20,7 +20,7 @@ object Config {
   val androidDependencyMode: FilePath = "androidTestImplementation"
   val androidDependencyGroup: String = "com.karumi"
   val androidDependencyName: String = "shot-android"
-  val androidDependencyVersion: String = "5.10.3-SNAPSHOT"
+  val androidDependencyVersion: String = "5.10.3"
   val androidDependency: FilePath =
     s"$androidDependencyGroup:$androidDependencyName:$androidDependencyVersion"
   def screenshotsFolderName(flavor: String, buildType: String): FilePath =
--- core/src/test/scala/com/karumi/shot/domain/ConfigSpec.scala
+++ core/src/test/scala/com/karumi/shot/domain/ConfigSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.{FlatSpec, Matchers}
 class ConfigSpec extends FlatSpec with Matchers {
 
   "Config" should "use the screenshot tests library implemented by Facebook" in {
-    Config.androidDependency shouldBe "com.karumi:shot-android:5.10.3-SNAPSHOT"
+    Config.androidDependency shouldBe "com.karumi:shot-android:5.10.3"
   }
 
   it should "add the dependency using the androidTestImplementation mode" in {
