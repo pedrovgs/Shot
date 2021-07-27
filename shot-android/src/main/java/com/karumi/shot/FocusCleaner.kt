ae76a4bd84924f4476f372c2a6661e7186d1aa7c
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2021-07-27 22:32:40 +0200

    Allow to focus view by Id or by tag

--- shot-android/src/main/java/com/karumi/shot/FocusCleaner.kt
+++ shot-android/src/main/java/com/karumi/shot/FocusCleaner.kt
@@ -4,12 +4,29 @@ import android.view.View
 
 interface FocusCleaner {
 
-    var focusTargetView: View?
+    sealed class FocusViewIdentifier {
+        class FocusById(val id: Int?) : FocusViewIdentifier()
+        class FocusByTag(val tag: Any?) : FocusViewIdentifier()
+    }
+
+    private fun View.meetsFocusCondition(focusViewIdentifier: FocusViewIdentifier): Boolean =
+        when (focusViewIdentifier) {
+            is FocusViewIdentifier.FocusById -> focusViewIdentifier.id == id
+            is FocusViewIdentifier.FocusByTag -> focusViewIdentifier.tag == tag
+        }
+
+    val focusTargetView: FocusViewIdentifier?
+        get() = null
 
     fun setFocusOnTargetView(view: View) {
         focusTargetView?.run {
-            view.filterChildrenViews { child -> child.isFocusable }.map { it.clearFocus() }
-            requestFocusFromTouch()
+            view.filterChildrenViews { child -> child.isFocusable }.forEach { it.clearFocus() }
+
+            val focusedView =
+                view.filterChildrenViews { child -> child.meetsFocusCondition(this) }
+                    .firstOrNull()
+
+            focusedView?.requestFocusFromTouch()
         }
     }
 }
