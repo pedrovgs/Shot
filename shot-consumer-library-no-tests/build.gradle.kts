52f32d3e7d7ebbeae76b33bb7d905504b61012b6
Pedro Gómez <pedro@karumi.com>
2021-08-05 16:10:36 +0200

    Move new sample project to another folder

---
+++ shot-consumer-library-no-tests/build.gradle.kts
@@ -0,0 +1,20 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+  repositories {
+    mavenLocal()
+    google()
+    mavenCentral()
+  }
+  dependencies {
+    classpath("com.android.tools.build:gradle:7.1.0-alpha06")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+    classpath("com.karumi:shot:5.11.2")
+
+    // NOTE: Do not place your application dependencies here; they belong
+    // in the individual module build.gradle.kts files
+  }
+}
+
+tasks.register("clean", Delete::class) {
+  delete(rootProject.buildDir)
+}
