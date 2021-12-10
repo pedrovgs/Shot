b9824139caa77f491f8298996d6d8b0246afb811
Pedro Gómez <pedro@goodnotesapp.com>
2021-12-10 12:33:07 +0100

    Prepare release 5.12.1

--- core/src/main/scala/com/karumi/shot/domain/model.scala
+++ core/src/main/scala/com/karumi/shot/domain/model.scala
@@ -16,7 +16,7 @@ object Config {
   val androidDependencyMode: FilePath  = "androidTestImplementation"
   val androidDependencyGroup: String   = "com.karumi"
   val androidDependencyName: String    = "shot-android"
-  val androidDependencyVersion: String = "5.12.0"
+  val androidDependencyVersion: String = "5.12.1"
   val androidDependency: FilePath =
     s"$androidDependencyGroup:$androidDependencyName:$androidDependencyVersion"
 
