a2614643d7c4d5d12f7ca887206f6b1f97025da0
Oleg Osipenko <oleg.s.osipenko@gmail.com>
2022-02-12 23:35:31 +0100

    Fixed json processing for Compose

--- core/src/main/scala/com/karumi/shot/json/ScreenshotsComposeSuiteJsonParser.scala
+++ core/src/main/scala/com/karumi/shot/json/ScreenshotsComposeSuiteJsonParser.scala
@@ -3,18 +3,31 @@ package com.karumi.shot.json
 import com.karumi.shot.domain.{Dimension, Screenshot}
 import com.karumi.shot.domain.model.{Folder, ScreenshotsSuite}
 import org.json4s._
-import org.json4s.jackson.JsonMethods._
+import org.json4s.native.JsonMethods._
 
 object ScreenshotsComposeSuiteJsonParser {
+
+  def parseScreenshot(jsonNode: JsonAST.JValue): ComposeScreenshot = {
+    val JString(name)          = jsonNode \ "name"
+    val JString(testClassName) = jsonNode \ "testClassName"
+    val JString(testName)      = jsonNode \ "testName"
+
+    ComposeScreenshot(name, testClassName, testName)
+  }
+
   def parseScreenshots(
-      json: String,
+      metadataJson: String,
       screenshotsFolder: Folder,
       temporalScreenshotsFolder: Folder,
       screenshotsTemporalBuildPath: Folder
   ): ScreenshotsSuite = {
     implicit val formats: DefaultFormats.type = DefaultFormats
-    val composeSuite                          = parse(json).extract[ComposeScreenshotSuite]
-    composeSuite.screenshots.map { screenshot =>
+
+    val json                            = parse(metadataJson)
+    val JObject(composeScreenshotSuite) = json
+    val JArray(composeScreenshots)      = json \ "screenshots"
+
+    composeScreenshots.map(parseScreenshot).map { screenshot =>
       val name = screenshot.name
       Screenshot(
         name = name,
